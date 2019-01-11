a592694d3d6a8c8f87f24282bf42e578db646845
kgkosta360 <kgkosta360@gmail.com>
2019-01-11 17:46:11 +0100

    obrisano

--- otvaranje.php
+++
@@ -1,8 +0,0 @@
-<?php
-$imefajla = $_POST['fajl'];
- 
-   header('Content-Disposition: inline; filename="'.$imefajla.'"');
-      header('Content-Type: application/pdf');
-      header('Content-Length: '.filesize($imefajla));
-      readfile($imefajla);
-?>
